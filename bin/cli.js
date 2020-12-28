93926ff08430831792e867ac7fe19c82df6c982b
wangguanghui <wangguanghui1@guazi.com>
2020-12-28 14:58:34 +0800

    fix: bug fix

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const path = require('path')
-const ejs = require('ejs')
+const ejs= require('ejs')
 const inquirer = require('inquirer')
 
 inquirer.prompt([{
@@ -18,9 +18,20 @@ inquirer.prompt([{
     message: 'project name'
 }]).then((answer) => {
     const { name, type } = answer
-    const tempDir = path.join(__dirname, `../template/${type}`)
-    const distDir = process.cwd()
-    generateDist(tempDir, distDir, answer)
+    /**
+     * todo:
+     * 1. 在当前工作目录下创建项目文件夹， 把该文件夹的绝对路径作为文件拷贝的目的路径
+     * 2. 将template中的文件使用ejs渲染， 同时拷贝到目的路径
+     * 
+     */
+    try {
+        const distDir = `${process.cwd()}/${name}`
+        fs.mkdirSync(distDir)
+        const tempDir = path.join(__dirname, `../template/${type}`)
+        generateDist(tempDir, distDir, answer)
+    } catch(e) {
+        console.log(e.message)
+    }
 })
 function generateDist(tempDir, distDir, answer) {
     try {
